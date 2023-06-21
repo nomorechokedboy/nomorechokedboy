00de212e7f00d1e68a60fd7912e55e878e5d8700
nomorechokedboy <haiduongit@protonmail.com>
2023-06-21 18:12:26 +0700

    ci: fixed vercel error #1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import vercel from 'solid-start-vercel'
 import solid from 'solid-start/vite'
 import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [
-		solid(),
+		solid({ adapter: vercel({ edge: true }) }),
 		Icons({
 			compiler: 'solid'
 		})
