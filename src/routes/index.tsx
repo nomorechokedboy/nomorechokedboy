2fcc186029a298515ab519c5729b0a392c99992c
nomorechokedboy <haiduongit@protonmail.com>
2023-06-22 08:43:57 +0700

    feat: completed about section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { createEffect } from 'solid-js'
 import { A, useLocation } from 'solid-start'
+import About from '~/sections/About'
 import Hero from '~/sections/Hero'
 
 export default function Home() {
 	return (
 		<main class='w-full h-full'>
 			<Hero />
+			<About />
 		</main>
 	)
 }
---
+++ src/sections/About.tsx
@@ -0,0 +1,60 @@
+import AboutImage from '~/assets/about.gif'
+import DownloadIcon from '~icons/line-md/download-loop'
+
+export default function About() {
+	return (
+		<section class='flex flex-col gap-5 p-5'>
+			<header class='flex flex-col gap-3 items-center'>
+				<h2 class='font-bold text-5xl text-black dark:text-white'>
+					About me
+				</h2>
+				<p class='font-semibold text-2xl text-neutral-600 dark:text-neutral-400'>
+					My introduction
+				</p>
+			</header>
+			<div class='flex flex-col-reverse gap-5 md:flex-row'>
+				<figure class='flex-1'>
+					<img
+						class='max-w-full h-auto'
+						width={410}
+						height={350}
+						src={AboutImage}
+						alt='This is me enjoying my deadline. Joking!'
+					/>
+				</figure>
+				<div class='flex-1 flex flex-col gap-3'>
+					<p class='font-medium text-lg text-neutral-600 dark:text-neutral-400'>
+						Lorem ipsum dolor sit amet
+						consectetur. Tincidunt
+						consectetur urna eget posuere at
+						integer arcu tempor. Faucibus
+						aenean in aliquet ut
+						consectetur. Scelerisque
+						pellentesque cras diam purus.
+						Nec ut proin scelerisque
+						faucibus vestibulum eu ipsum sit
+						semper.
+					</p>
+					<div>
+						<button class='bg-neutral-900 px-5 py-2 rounded-lg dark:bg-white'>
+							<div class='flex items-center gap-2 justify-center'>
+								<DownloadIcon
+									width={
+										24
+									}
+									height={
+										24
+									}
+								/>
+								<span class='text-white text-lg font-medium dark:text-neutral-950'>
+									Download
+									CV
+								</span>
+							</div>
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
