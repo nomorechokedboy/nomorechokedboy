88b87cd7d7e3b72db8fd7478636371e84cff78eb
nomorechokedboy <haiduongit@protonmail.com>
2023-06-22 08:49:45 +0700

    feat: completed projects section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { createEffect } from 'solid-js'
 import { A, useLocation } from 'solid-start'
 import About from '~/sections/About'
 import Hero from '~/sections/Hero'
+import Projects from '~/sections/Projects'
 
 export default function Home() {
 	return (
 		<main class='w-full h-full'>
 			<Hero />
 			<About />
+			<Projects />
 		</main>
 	)
 }
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,11 @@
+export default function Projects() {
+	return (
+		<section>
+			<div class='py-14 text-center bg-neutral-50 dark:bg-[#111]'>
+				<span class='font-bold text-2xl text-black dark:text-white'>
+					New projects coming soon...
+				</span>
+			</div>
+		</section>
+	)
+}
