18099eabb712d9ebadb6a9da3beb7d92e2afa2df
nomorechokedboy <haiduongit@protonmail.com>
2023-09-30 12:35:02 +0700

    fix: fix ResumeProject

--- src/routes/resume/ResumeProject.tsx
+++ src/routes/resume/ResumeProject.tsx
@@ -1,4 +1,4 @@
-import { For } from 'solid-js'
+import { For, Show } from 'solid-js'
 import { unstable_clientOnly } from 'solid-start'
 import Skill from './Skill'
 import styles from './styles.module.css'
@@ -17,12 +17,24 @@ export default function ResumeProject(props: ProjectProps) {
 		<div class='flex flex-col gap-1'>
 			<header class='flex flex-col md:flex-row md:items-center md:justify-between print:flex-row print:items-center print:justify-between'>
 				<h2 class='font-bold'>
-					<a
-						class='underline underline-offset-2 decoration-2 decoration-sky-600 hover:text-blue-600 md:text-center print:text-center'
-						href={props.url}
+					<Show
+						when={
+							props.url &&
+							props.url !== ''
+						}
+						fallback={
+							<span>
+								{props.name}
+							</span>
+						}
 					>
-						{props.name}
-					</a>
+						<a
+							class='underline underline-offset-2 decoration-2 decoration-sky-600 hover:text-blue-600 md:text-center print:text-center'
+							href={props.url}
+						>
+							{props.name}
+						</a>
+					</Show>
 					&nbsp;
 					<span class='inline-flex gap-1 items-center'>
 						<For each={props.technologies}>
